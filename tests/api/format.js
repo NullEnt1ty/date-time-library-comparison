6db649a7553d1d3b61536772304ef949e3256324
Dominique Mattern <dominique.mattern@5minds.de>
2017-06-06 08:33:26 +0200

    :sparkles: Add formatting for js-joda

--- tests/api/format.js
+++ tests/api/format.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const moment = require('moment');
+const ZonedDateTime = require('js-joda').ZonedDateTime;
+const DateTimeFormatter = require('js-joda').DateTimeFormatter;
 const fnsFormat = require('date-fns/format');
 
 const date = '2017-04-21T11:04:49Z';
@@ -9,7 +11,8 @@ const date = '2017-04-21T11:04:49Z';
 const momentDate = moment(date).format('DD.MM.YYYY HH:mm'); // 21.04.2017 13:04
 
 // js-joda
-// I couldn't find any method for formatting a date in the documentation
+const formatter = DateTimeFormatter.ofPattern('dd.MM.yyyy HH:mm');
+const jodaDate = formatter.format(ZonedDateTime.parse(date)); // 21.04.2017 11:04
 
 // date-fns
 const fnsDate = fnsFormat(date, 'DD.MM.YYYY HH:mm'); // 21.04.2017 13:04
